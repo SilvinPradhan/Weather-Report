a871beb33bd22a6c76c7df952bb589ddd89229ae
SilvinPradhan <silvinpradhan95@gmail.com>
2020-06-19 12:10:49 -0500

    Deploy weather finding app into github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Weather from "./Weather";
+import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+  /* images */
 
-function App() {
+const apiKey = "8ddceeacaf8b95fe943c88fc8389dee0";
+
+const Title = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title-container__title">
+        Weather Finder
+      </h1>
+      <h3 className="title-container__subtitle">
+        Find out temperature, conditions and more...
+      </h3>
     </div>
   );
+};
+
+const Form = ({ onWeather }) => {
+  return (
+    <form onSubmit={e => onWeather(e)}>
+      <input type="text" name="city" placeholder="Enter a City" />
+      <input type="text" name="country" placeholder="Enter a Country" />
+      <button className="form-button">Get Weather</button>
+    </form>
+  );
+};
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.currentTarget.elements.city.value;
+    const country = e.currentTarget.elements.country.value;
+    if (city && country) {
+      try {
+        const apiCall = await fetch(
+          `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+        );
+        const { main, sys, name, weather } = await apiCall.json();
+        this.setState({
+          temperature: main.temp,
+          city: name,
+          country: sys.country,
+          humidity: main.humidity,
+          description: weather[0].description,
+          error: ""
+        });
+      } catch (ex) {
+        console.log(ex.message);
+      }
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter valid values!"
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main">
+          <div className="container" style={{ width: "100%" }}>
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Title />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form onWeather={this.getWeather} />
+                <Weather
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
