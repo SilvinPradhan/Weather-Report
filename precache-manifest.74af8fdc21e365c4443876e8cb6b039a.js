f5679d953f2d3bc873db59a481fc206c06abfff0
SilvinPradhan <silvinpradhan95@gmail.com>
2020-06-19 12:00:07 -0500

    Updates

---
+++ precache-manifest.74af8fdc21e365c4443876e8cb6b039a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d12a50026b8805aece78ec06384c97",
+    "url": "/Weather-Report/index.html"
+  },
+  {
+    "revision": "b6938d43c91629e922ae",
+    "url": "/Weather-Report/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "00012aedf04d5e9039b5",
+    "url": "/Weather-Report/static/css/main.1bde3fe4.chunk.css"
+  },
+  {
+    "revision": "b6938d43c91629e922ae",
+    "url": "/Weather-Report/static/js/2.07d0fa97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Report/static/js/2.07d0fa97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00012aedf04d5e9039b5",
+    "url": "/Weather-Report/static/js/main.6d4f4957.chunk.js"
+  },
+  {
+    "revision": "d3c8571fad47711cdfc1",
+    "url": "/Weather-Report/static/js/runtime-main.e3698bb4.js"
+  }
+]);
